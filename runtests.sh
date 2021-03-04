1d2c3cb3b61d7bfb31ebbafbcd214f0047d09f71
Dami <obayanjud@gmail.com>
2021-03-04 00:08:41 -0700

    Remove all traces of secret keys

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export PAYSTACK_KEY=sk_test_b748a89ad84f35c2f1a8b81681f956274de048bb
 
 go vet $(go list ./... | grep -v vendor)
 test -z "$(golint ./... | grep -v vendor | tee /dev/stderr)"
