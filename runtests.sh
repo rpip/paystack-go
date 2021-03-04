041781a8198dacbae6c7cdcc12d7ab03be5b9401
Dami <obayanjud@gmail.com>
2021-03-04 00:09:37 -0700

    Go command to install 3rd party packages

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+go get ./...
 go vet $(go list ./... | grep -v vendor)
 test -z "$(golint ./... | grep -v vendor | tee /dev/stderr)"
 test -z "$(gofmt -s -l . | grep -v vendor | tee /dev/stderr)"
